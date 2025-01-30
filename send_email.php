124b31339623e281261ad47384f9cf719d74efe9
ZloyGolub <45264265+ZloyGolub@users.noreply.github.com>
2025-01-30 04:16:52 +0100

    Update Get a book
    The form works via email php

---
+++ send_email.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Подключаем PHPMailer
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST["firstname"]);
+    $lastName = htmlspecialchars($_POST["lastname"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $address = htmlspecialchars($_POST["address"]);
+    $postcode = htmlspecialchars($_POST["postcode"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $mail = new PHPMailer(true);
+    try {
+        // Настройки SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // SMTP сервер (можно заменить на SMTP другого сервиса)
+        $mail->SMTPAuth = true;
+        $mail->Username = 'zloygolub@gmail.com'; // Замени на свой email
+        $mail->Password = 'zzqm kahf fcya yngw'; // Пароль от email (лучше использовать App Password)
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+
+        // От кого письмо
+        $mail->setFrom($email, "$firstName $lastName");
+        // Кому отправляем
+        $mail->addAddress("kolosova.vell@gmail.com"); // Замени на свою почту
+
+        // Тема письма
+        $mail->Subject = "New Book Request from $firstName $lastName";
+        // Тело письма
+        $mail->Body = "You have received a new book request.\n\n" .
+                      "Name: $firstName $lastName\n" .
+                      "Email: $email\n" .
+                      "Address: $address\n" .
+                      "Postcode: $postcode\n" .
+                      "Message:\n$message\n";
+
+        // Отправка
+        $mail->send();
+        echo "success";
+    } catch (Exception $e) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+} else {
+    header("Location: index.html");
+    exit();
+}
+?>
